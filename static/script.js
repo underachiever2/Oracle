baecf2537ea13559d63c264b9bb499549027621b
underachiever2 <mrjohnson88@ymail.com>
2024-08-26 19:03:32 +0000

    Pushing all current files including scripts.js and other updates

---
+++ static/script.js
@@ -0,0 +1,15 @@
+function uploadChart() {
+    alert("Upload Chart button clicked!");
+    // Add logic to handle file upload
+}
+
+function signUp() {
+    alert("Sign Up button clicked!");
+    // Add logic for sign-up functionality
+}
+
+function signIn() {
+    alert("Sign In button clicked!");
+    // Add logic for sign-in functionality
+}
+
